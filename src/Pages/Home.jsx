508eb04d91a86f7dca93a358518ae4ccdad5afe3
hafidznrg <hafidznrg.edu@gmail.com>
2021-11-20 11:00:59 +0700

    finish homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,21 @@
-import React,{useState,useEffect} from 'react';
+import React,{ useState } from 'react';
 import styles from "./home.module.css";
 import MovieData from '../public/MovieData';
+import { MdDelete } from "react-icons/md";
 
 function Home() {
   const [wishlist, setWishlist] = useState([]);
-  const handleClick = (name) =>{
-    setWishlist(wishlist => [...wishlist, name]);
+  const handleClick = (item) => {
+    if (!wishlist.includes(item)){
+      setWishlist(wishlist => [...wishlist, item]);
+    }
+  }
+
+  const handleDelete = (item) => {
+    const newWish = wishlist.filter((wishItem) => {
+      return wishItem.id !== item.id
+    })
+    setWishlist(newWish)
   }
 
   return (
@@ -17,26 +27,31 @@ function Home() {
                 <div className = {styles.content}>
                     {
                         MovieData && MovieData.length>0 && MovieData.map((item)=> {
-                        var poster = item.poster;
                         return (
-                            <div key = {item.nama} className = {styles.movieItem}>
+                            <div key = {item.id} className = {styles.movieItem}>
                                 <div className = {styles.movieImg}>
-                                    <img src = {require(`${poster}`)}/>
+                                    <img src = {item.poster.default}/>
                                 </div>
                                 <h3 className = {styles.movieTitle}>{item.nama}</h3>
-                                <p className = {styles.movieDesc}>{item.desc}</p>
-                                <button onClick={() => handleClick(item.nama)}>Click</button>
+                                <p className = {styles.movieDesc}>{item.desc.slice(0,60)}..</p>
+                                <i>See details</i>
+                                <button onClick={() => handleClick(item)} className = {styles.btn}>Add to wishlist</button>
                             </div>
                         )})
                     }
                 </div>
             </div>
             <div className={styles.containerRight}>
-                <h2>Wishlist</h2>
+            <div className = {styles.title}>
+                    <h2>Your Wishlist</h2>
+                </div>
                 {
                   wishlist.map((item) =>{
                     return (
-                      <p>{item}</p>
+                      <div key= {item.id} className={styles.wishItem}>
+                        <h4>{item.nama}</h4>
+                        <button onClick= {() => handleDelete(item)} className = {styles.btn}><MdDelete/></button>
+                      </div>
                     )
                   })
                 }
--- src/public/MovieData.js
+++ src/public/MovieData.js
@@ -1,8 +1,9 @@
 const MovieData =
         [
             {     
+                "id" : 1,
                 "nama" : "#FriendButMarried 2",
-                "poster" : "./img/FriendButMarried2.jpg",
+                "poster" : require("../public/img/FriendButMarried2.jpg"),
                 "tahun" : 2020,
                 "desc" : "Ayudia (Mawar De Jongh) is not satisfied enough in enjoying the moment as a wife, but she suddenly becomes pregnant. Her personality changes completely and becomes lazy and super sensitive.",
                 "genre" : "Biography",
@@ -11,8 +12,9 @@ const MovieData =
                 "director" : "Rako Prijanto"
             },
             {     
+                "id" : 2,
                 "nama" : "Aku Tahu Kapan Kamu Mati",
-                "poster" : "./img/AkuTahuKapanKamuMati.jpg",
+                "poster" : require("../public/img/AkuTahuKapanKamuMati.jpg"),
                 "tahun" : 2020,
                 "desc" : "After apparent death, Siena is able to see signs that people will die. However, her friends did not believe in her abilities. Then, the sign appeared on her and those closest to her.",
                 "genre" : "Horror",
@@ -20,8 +22,9 @@ const MovieData =
                 "duration" : 92
             },
             {     
+                "id" : 3,
                 "nama" : "Dignitate",
-                "poster" : "./img/Dignitate.jpg",
+                "poster" : require("../public/img/Dignitate.jpg"),
                 "tahun" : 2020,
                 "desc" : "Alfi (Al Ghazali) meets Alana (Caitlin Halderman), a new student at school. It turns out that they are in one class and sitting next to each other. Instead of getting along, they often argue because they both are stubborn.",
                 "genre" : "Drama",
@@ -30,8 +33,9 @@ const MovieData =
                 "director" : "Fajar Nugros"
             },
             {     
+                "id" : 4,
                 "nama" : "Guru-Guru Gokil",
-                "poster" : "./img/GuruGuruGokil.jpg",
+                "poster" : require("../public/img/GuruGuruGokil.jpg"),
                 "tahun" : 2020,
                 "desc" : "When staff salaries get stolen at his school, a reluctant new teacher sets out to recoup the money and soon discovers the joys of teaching.",
                 "genre" : "Comedy",
@@ -40,8 +44,9 @@ const MovieData =
                 "director" : "Sammaria Simanjuntak"
             },
             {     
+                "id" : 5,
                 "nama" : "Nanti Kita Cerita Tentang Hari Ini",
-                "poster" : "./img/NKCTHI.jpg",
+                "poster" : require("../public/img/NKCTHI.jpg"),
                 "tahun" : 2020,
                 "desc" : "Three siblings live in happy-looking families, but one of them had changed and got warned by his parents. This prompted the rebellion of the three siblings which led to the discovery of the secrets and great trauma in their families.",
                 "genre" : "Drama",
@@ -50,8 +55,9 @@ const MovieData =
                 "director" : "Angga Dwimas Sasongko"
             },
             {     
+                "id" : 6,
                 "nama" : "After Met You",
-                "poster" : "./img/AfterMetYou.jpg",
+                "poster" : require("../public/img/AfterMetYou.jpg"),
                 "tahun" : 2019,
                 "desc" : "Ari, the coolest and most handsome guy at his school, is challenged by his friends to find a girlfriend who is definitely not interested in him.",
                 "genre" : "Drama",
@@ -60,8 +66,9 @@ const MovieData =
                 "director" : "Patrick Effendy"
             },
             {     
+                "id" : 7,
                 "nama" : "Antologi Rasa",
-                "poster" : "./img/AntologiRasa.jpg",
+                "poster" : require("../public/img/AntologiRasa.jpg"),
                 "tahun" : 2019,
                 "desc" : "Keara (Carissa Perusset) loves Ruly (Refal Hady), even though she realizes that Ruly loves Denise (Atikah Suhaime). Meanwhile, Harris (Herjunot Ali) also has found his true love in Keara. But for Keara, Harris is just her best friend.",
                 "genre" : "Drama",
@@ -70,8 +77,9 @@ const MovieData =
                 "director" : "Rizal Mantovani"
             },
             {     
+                "id" : 8,
                 "nama" : "Danur 3: Sunyaruri",
-                "poster" : "./img/Danur3.jpg",
+                "poster" : require("../public/img/Danur3.jpg"),
                 "tahun" : 2019,
                 "desc" : "After years of being friendly with her little ghosts, a girl begins to feel that she must have a normal life like other girls. Especially, now she has a boyfriend, but she doesn't tell him about her ability to see ghosts.",
                 "genre" : "Horror",
@@ -80,8 +88,9 @@ const MovieData =
                 "director" : "Awi Suryadi"
             },
             {     
+                "id" : 9,
                 "nama" : "Dilan 1991",
-                "poster" : "./img/Dilan1991.jpg",
+                "poster" : require("../public/img/Dilan1991.jpg"),
                 "tahun" : 2019,
                 "desc" : "Dilan and Milea officially date. But Dilan is threatened to be expelled from school due to involves in gang fights. One day, when he is planning to fight again, Milea asks him to quit the motorcycle gang or their relationship ends.",
                 "genre" : "Drama",
@@ -90,8 +99,9 @@ const MovieData =
                 "director" : "Pidi Baiq"
             },
             {     
+                "id" : 10,
                 "nama" : "Habibie & Ainun 3",
-                "poster" : "./img/HabibieAinun3.jpg",
+                "poster" : require("../public/img/HabibieAinun3.jpg"),
                 "tahun" : 2019,
                 "desc" : "This movie follows the story of Hasri Ainun Besari Habibie's young life when she was in high school and medical college. She was known as a smart figure who became an idol and admired by many male students, including B.J. Habibie.",
                 "genre" : "Biography",
@@ -100,8 +110,9 @@ const MovieData =
                 "director" : "Hanung Bramantyo"
             },
             {     
+                "id" : 11,
                 "nama" : "Ajari Aku Islam",
-                "poster" : "./img/AjariAkuIslam.jpg",
+                "poster" : require("../public/img/AjariAkuIslam.jpg"),
                 "tahun" : 2019,
                 "desc" : "Kenny (Roger Danuarta), a Medan Chinese young man fell in love with Fidya (Cut Meyriska) a Batak Melayu Muslim girl. Over time, they love each other, but they were also hit by cultural and religious differences.",
                 "genre" : "Drama",
@@ -110,8 +121,9 @@ const MovieData =
                 "director" : "Deni Pusung"
             },
             {     
+                "id" : 12,
                 "nama" : "Imperfect",
-                "poster" : "./img/Imperfect.jpg",
+                "poster" : require("../public/img/Imperfect.jpg"),
                 "tahun" : 2019,
                 "desc" : "Being born fat and has dark skins, it feels like a curse for Rara, especially when she worked at the office that has surrounded by pretty girls. Her boss wants her to lose her weight, but there is a man who loves the way she were.",
                 "genre" : "Comedy",
@@ -120,8 +132,9 @@ const MovieData =
                 "director" : "Ernest Prakasa"
             },
             {     
+                "id" : 13,
                 "nama" : "Laundry Show",
-                "poster" : "./img/LaundryShow.jpg",
+                "poster" : require("../public/img/LaundryShow.jpg"),
                 "tahun" : 2019,
                 "desc" : "A man chooses to resign from his office to set up a laundry business and has to deal with his employees who have various ridiculous personalities.",
                 "genre" : "Comedy",
@@ -130,8 +143,9 @@ const MovieData =
                 "director" : "Rizki Balki"
             },
             {     
+                "id" : 14,
                 "nama" : "Single 2",
-                "poster" : "./img/Single2.jpg",
+                "poster" : require("../public/img/Single2.jpg"),
                 "tahun" : 2019,
                 "desc" : "Ebi is going towards 30 years old and still single. Now, Ebi is determined that before his 30th birthday he must release his single status. He must have a girlfriend. Even though he doesn't know how to do it at all.",
                 "genre" : "Comedy",
@@ -140,8 +154,9 @@ const MovieData =
                 "director" : "Raditya Dika"
             },
             {     
+                "id" : 15,
                 "nama" : "Susi Susanti: Love All",
-                "poster" : "./img/SusiSusanti.jpg",
+                "poster" : require("../public/img/SusiSusanti.jpg"),
                 "tahun" : 2019,
                 "desc" : "Susi Susanti rises through the rank to become Indonesia's most beloved athlete. In time of economic turmoil, Susi seizes the opportunity to show her country and the world that heroism is measured by the depth of one's sacrifice.",
                 "genre" : "Biography",
@@ -150,8 +165,9 @@ const MovieData =
                 "director" : "Sim F."
             },
             {     
+                "id" : 16,
                 "nama" : "Yowis Ben 2",
-                "poster" : "./img/YowesBen2.jpg",
+                "poster" : require("../public/img/YowesBen2.jpg"),
                 "tahun" : 2019,
                 "desc" : "The popularity of Yowis Ben in Malang doesn't resolve Bayu's financial problems. The other members also have their own problems. Then they are introduced to Cak Jim who claims can raise them become a national artist.",
                 "genre" : "Comedy",
